82910be0e2f9c7785542bf2f20da5f57ef4a839e
bbelen111 <187245515+bbelen111@users.noreply.github.com>
2025-06-20 00:12:25 +0800

    feat: add query parameter for finding student by ID

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,7 @@
 import { Get } from '@nestjs/common';
 import { Controller } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
-import { ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { ApiOperation, ApiQuery, ApiResponse } from '@nestjs/swagger';
 import { ReturnStudentDto } from 'src/users/dto/students.dto';
 import { Student } from '@prisma/client'
 import { Query } from '@nestjs/common';
@@ -25,7 +25,9 @@ export class UsersController {
         return await this.userService.findAllStudents();
     }
 
+
     @Get('students/find')
+    @ApiQuery({ name: 'id', type: String, description: 'Student ID' })
     async findStudentById(
         @Query('id') id: Student['studentId']
     ) {
